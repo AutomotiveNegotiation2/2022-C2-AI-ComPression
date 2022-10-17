1dd8167e8e13e3ffac6271a33ecb343cf45527b5
NOHYC <nyc1517@gmail.com>
2022-10-17 12:14:04 +0900

    add setup.sh

---
+++ Rule_based_compression/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update -y && apt upgrade -y && apt-get install python3-pip -y
+apt-get install -y libsm6 libxext6 libxrender-dev
+apt install libgl1-mesa-glx
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+pip3 install open3d
+
+echo "ENV_SETUP_DONE"
