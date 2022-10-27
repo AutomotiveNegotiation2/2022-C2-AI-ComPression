4d76a614e12fda082408d12db2d70f6d37819cc8
NOHYC <nyc1517@gmail.com>
2022-10-27 17:27:34 +0900

    add update

--- Hybrid_compression/docker_setup.sh
+++ Hybrid_compression/docker_setup.sh
@@ -6,8 +6,9 @@ apt-get install bc -y
 apt-get install python3-pip -y
 apt-get install wget -y
 
+
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
 
 pip3 install torch torchvision
-pip3 install argparse pandas numpy scipy scikit-learn tqdm
+pip3 install argparse pandas numpy scipy scikit-learn tqdm matplotlib 
