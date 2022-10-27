a1ddf79bf26dc87d80da1899fcaa206e7b06dde1
NOHYC <nyc1517@gmail.com>
2022-10-27 18:03:35 +0900

    change torch vertion and Tkinter

--- Hybrid_compression/docker_setup.sh
+++ Hybrid_compression/docker_setup.sh
@@ -10,5 +10,5 @@ apt-get install wget -y
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
 
-pip3 install torch torchvision
+pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip3 install argparse pandas numpy scipy scikit-learn tqdm matplotlib 
