8b3de20e500769bb2ea70c68332c39a3e817b1b8
NOHYC <nyc1517@gmail.com>
2022-10-27 17:01:48 +0900

    add doceker_setup

---
+++ Hybrid_compression/docker_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install bc -y
+apt-get install python3-pip -y
+apt-get install wget -y
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+
+pip3 install torch torchvision
+pip3 install argparse pandas numpy scipy scikit-learn tqdm
