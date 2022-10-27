01a0c3c076fae51a0d8eb778a24e7401b68dd77f
NOH <nyc1517@gmail.com>
2022-10-27 22:59:27 +0900

    check ERROR

--- Rule_based_compression/Voxelization_compression.py
+++ Rule_based_compression/Voxelization_compression.py
@@ -47,69 +47,80 @@ if __name__ == "__main__":
     
     ctr = vis.get_view_control()
     ctr2 = vis2.get_view_control()
-    param = o3d.io.read_pinhole_camera_parameters(CAMERA_PARAM)
-    icp_iteration = len(data)
-    # plt.figure(figsize=(5,5))
-    fig,ax = plt.subplots(ncols=1,nrows=3,gridspec_kw={'height_ratios' : [3,1,1]}, figsize=(7,15))
-    for j in range(icp_iteration):
-        time.sleep(0.001)
+    if os.path.isfile(CAMERA_PARAM) and os.path.isfile(VIEW_RANDER_PARAM):
+        print("LOAD_CAMERA_CONFIG_AND_RENDERING_OPT")
 
-        vis.add_geometry(data[j])
-        ctr.convert_from_pinhole_camera_parameters(param)
-        vis.get_render_option().load_from_json(VIEW_RANDER_PARAM)
-        vis.poll_events()
-        vis.update_renderer()
-        vis.clear_geometries()
-        #vis.remove_geometry(data[j])
+        param = o3d.io.read_pinhole_camera_parameters(CAMERA_PARAM)
+        icp_iteration = len(data)
+        # plt.figure(figsize=(5,5))
+        fig,ax = plt.subplots(ncols=1,nrows=3,gridspec_kw={'height_ratios' : [3,1,1]}, figsize=(7,15))
+        for j in range(icp_iteration):
+            time.sleep(0.001)
+
+            vis.add_geometry(data[j])
+            ctr.convert_from_pinhole_camera_parameters(param)
+            vis.get_render_option().load_from_json(VIEW_RANDER_PARAM)
+            vis.poll_events()
+            vis.update_renderer()
+            vis.clear_geometries()
+            #vis.remove_geometry(data[j])
+            
+            downpcd = data[j].voxel_down_sample(voxel_size=voxel_size_param)
+            o3d.io.write_point_cloud("./Down_pcd.pcd",downpcd)
+            vis2.add_geometry(downpcd)
+            ctr2.convert_from_pinhole_camera_parameters(param)
+            vis2.get_render_option().load_from_json(VIEW_RANDER_PARAM)
+            vis2.poll_events()
+            vis2.update_renderer()
+            vis2.clear_geometries()
+            origin_pcd_size = os.path.getsize( os.path.join(PCD_DATA_PATH,pcd_data_list[j]))
+            down_pcd_size = os.path.getsize("./Down_pcd.pcd")
+            if os.path.exists("./Down_pcd.pcd"):
+                os.remove("./Down_pcd.pcd")
+
+            space_savings = (1 - (down_pcd_size / origin_pcd_size) )*100
+            img_test = img.imread(os.path.join(DATA_PATH,image_list[j]))
+            count_q.append(j)
+            saving_space_q.append(space_savings)
+            raw_point_cloud_q.append(len(np.asarray(data[j].points)))
+            voxel_point_cloud_q.append(len(np.asarray(downpcd.points)))
+
+            if len(count_q) > 20:
+                count_q.popleft()
+                saving_space_q.popleft()
+                raw_point_cloud_q.popleft()
+                voxel_point_cloud_q.popleft()
+
+            
+            ax[0].cla()
+            ax[0].text(1200.0,0.0,"Space_savings : " + str(space_savings)[:5]+"%")
+            ax[0].imshow(img_test)
+            ax[0].axis("off")
+
+            ax[1].cla()
+            ax[1].plot(count_q,raw_point_cloud_q,label = "ORIGIN_PCD[num]")
+            ax[1].plot(count_q,voxel_point_cloud_q,label="VOXELIZATION_PCD[num]")
+            ax[1].set_title("POINT CLOUD COUNT GRAPH")
+            ax[1].legend(fontsize = 8)
+            ax[1].set(ylabel = "COUNT",xlabel = "TIME")
+
+            ax[2].cla()
+            ax[2].plot(count_q,saving_space_q,label="SPACE_SAVINGS[%]")
+            ax[2].text(count_q[-1],saving_space_q[-1],str(space_savings)[:4] +"%",color = "red", fontsize = 10)
+            ax[2].set_title("COMPRESSION GRAPH")
+            ax[2].legend(fontsize = 8)
+            ax[2].set(ylabel = "SPACE_SAVINGS(%)",xlabel = "TIME")
+            ax[2].set_ylim([60,100])
+            plt.tight_layout()
+            plt.pause(0.001)
+        vis.destroy_window()
+        vis2.destroy_window()
+        plt.close()
+
         
-        downpcd = data[j].voxel_down_sample(voxel_size=voxel_size_param)
-        o3d.io.write_point_cloud("./Down_pcd.pcd",downpcd)
-        vis2.add_geometry(downpcd)
-        ctr2.convert_from_pinhole_camera_parameters(param)
-        vis2.get_render_option().load_from_json(VIEW_RANDER_PARAM)
-        vis2.poll_events()
-        vis2.update_renderer()
-        vis2.clear_geometries()
-        origin_pcd_size = os.path.getsize( os.path.join(PCD_DATA_PATH,pcd_data_list[j]))
-        down_pcd_size = os.path.getsize("./Down_pcd.pcd")
-        if os.path.exists("./Down_pcd.pcd"):
-            os.remove("./Down_pcd.pcd")
-
-        space_savings = (1 - (down_pcd_size / origin_pcd_size) )*100
-        img_test = img.imread(os.path.join(DATA_PATH,image_list[j]))
-        count_q.append(j)
-        saving_space_q.append(space_savings)
-        raw_point_cloud_q.append(len(np.asarray(data[j].points)))
-        voxel_point_cloud_q.append(len(np.asarray(downpcd.points)))
-
-        if len(count_q) > 20:
-            count_q.popleft()
-            saving_space_q.popleft()
-            raw_point_cloud_q.popleft()
-            voxel_point_cloud_q.popleft()
-
+    else:
+        if not os.path.isfile(CAMERA_PARAM):
+            print("CHECK_CAMERA_CONFIG")
+        else:
+            print("CHECK_RENDERING_OPT")
         
-        ax[0].cla()
-        ax[0].text(1200.0,0.0,"Space_savings : " + str(space_savings)[:5]+"%")
-        ax[0].imshow(img_test)
-        ax[0].axis("off")
-
-        ax[1].cla()
-        ax[1].plot(count_q,raw_point_cloud_q,label = "ORIGIN_PCD[num]")
-        ax[1].plot(count_q,voxel_point_cloud_q,label="VOXELIZATION_PCD[num]")
-        ax[1].set_title("POINT CLOUD COUNT GRAPH")
-        ax[1].legend(fontsize = 8)
-        ax[1].set(ylabel = "COUNT",xlabel = "TIME")
-
-        ax[2].cla()
-        ax[2].plot(count_q,saving_space_q,label="SPACE_SAVINGS[%]")
-        ax[2].text(count_q[-1],saving_space_q[-1],str(space_savings)[:4] +"%",color = "red", fontsize = 10)
-        ax[2].set_title("COMPRESSION GRAPH")
-        ax[2].legend(fontsize = 8)
-        ax[2].set(ylabel = "SPACE_SAVINGS(%)",xlabel = "TIME")
-        ax[2].set_ylim([60,100])
-        plt.tight_layout()
-        plt.pause(0.001)
-    vis.destroy_window()
-    vis2.destroy_window()
-    plt.close()
