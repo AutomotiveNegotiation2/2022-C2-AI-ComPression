d862163446c68045e9f634eaa6f0a91ee91237d0
NOHYC <nyc1517@gmail.com>
2022-10-27 18:06:49 +0900

    change torch version and add tkinter

--- Hybrid_compression/setup.sh
+++ Hybrid_compression/setup.sh
@@ -5,9 +5,10 @@ sudo apt-get upgrade -y
 sudo apt-get install bc -y
 sudo apt-get install python3-pip -y
 sudo apt-get install wget -y
+sudo apt-get install python3-tk -y
 
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
 
-pip3 install torch torchvision
-pip3 install argparse pandas numpy scipy scikit-learn tqdm
+pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install argparse pandas numpy scipy scikit-learn tqdm matplotlib 
