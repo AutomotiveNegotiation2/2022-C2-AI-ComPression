cc53e10ac7abd21580fe88f8ebc7921507a02226
NOHYC <nyc1517@gmail.com>
2022-10-27 17:00:45 +0900

    add setup.sh update

--- Hybrid_compression/setup.sh
+++ Hybrid_compression/setup.sh
@@ -6,5 +6,8 @@ sudo apt-get install bc -y
 sudo apt-get install python3-pip -y
 sudo apt-get install wget -y
 
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+
 pip3 install torch torchvision
 pip3 install argparse pandas numpy scipy scikit-learn tqdm
