bd750726e195f08e4403a563e8c08ff2a41a823c
NOHYC <nyc1517@gmail.com>
2022-10-28 16:12:12 +0900

    add curl down

--- Rule_based_compression/docker_setup.sh
+++ Rule_based_compression/docker_setup.sh
@@ -4,6 +4,8 @@ apt update -y && apt upgrade -y && apt-get install python3-pip -y
 apt-get install -y libsm6 libxext6 libxrender-dev
 apt install libgl1-mesa-glx -y
 apt-get install curl -y
+
+
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
 pip3 install open3d
