e05e4bd8bf99884e635c2e6945f29f9a1ac37293
NOHYC <nyc1517@gmail.com>
2022-11-08 13:32:31 +0900

    fix pip open3d version

--- Rule_based_compression/setup.sh
+++ Rule_based_compression/setup.sh
@@ -6,6 +6,6 @@ sudo apt install libgl1-mesa-glx -y
 sudo apt-get install curl -y
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
-pip3 install open3d
+pip3 install open3d==0.15.2
 
 echo "ENV_SETUP_DONE"
