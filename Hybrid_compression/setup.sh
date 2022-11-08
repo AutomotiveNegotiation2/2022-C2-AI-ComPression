189e3c1efd8f6c060ea3fa84c857e2a24b146384
NOHYC <nyc1517@gmail.com>
2022-11-08 13:29:09 +0900

    fix pip install requirements.txt

--- Hybrid_compression/setup.sh
+++ Hybrid_compression/setup.sh
@@ -12,4 +12,5 @@ python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
 
 pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip3 install argparse pandas numpy scipy scikit-learn tqdm matplotlib 
+pip3 install -r requirements.txt
+
