9f97fbb833caffb5709d3de370fc75ee84f31565
NOHYC <nyc1517@gmail.com>
2024-11-01 17:02:37 +0900

    add parser_gps

---
+++ SensorData_Parser/GPS_read.py
@@ -0,0 +1,28 @@
+import serial
+from pyubx2 import UBXReader
+import time
+
+def read_data(share_list, port_name = "/dev/ttyACM2",baudrate= 115200) :
+    delta_t = 0
+    data_str = ""
+    print("START_GPS")
+
+    while True:
+        if delta_t > 1.0:
+            share_list[3] = data_str
+            data_str = ""
+            delta_t = 0  
+        before_recv_t = time.time()              
+        stream = serial.Serial(port_name, baudrate, timeout = 3)
+        ubr = UBXReader(stream,protfilter = 1)
+        (raw_data, _) = ubr.read()
+        data_str += raw_data.decode()
+        data_str += "\n"
+        after_recv_t = time.time()
+        delta_t += (after_recv_t - before_recv_t)
+        
+
+if __name__  == "__main__":
+    data_list = [[] for _ in range(6)]
+    read_data(data_list,"/dev/ttyACM0",115200)
+    
