493d897001407aa164df3ec64735e97dc20a59ce
NOHYC <nyc1517@gmail.com>
2024-11-08 14:00:55 +0900

    add dem_process

--- SensorData_Parser/DEM_gen.py
+++ SensorData_Parser/DEM_gen.py
@@ -19,3 +19,16 @@ cam04_H = np.load(config["camera_matrices"]["cam04_H"])
 DEG_TO_RAD = config["constants"]["DEG_TO_RAD"]
 canfd_dict = config["canfd_dict"]
 cam_det_px_dict = config["cam_det_px_dict"]
+
+def process_camera_objects(cam_pos, cam_index, cam_objs, homography, angle_offset, det_px_dict_key,mylat, mylon, my_head):
+    global cam_obj_gps, cam_obj
+    cam_pos[cam_index] = 1
+    for obj in cam_objs:
+        obj_class, det_x, det_y = obj
+        px, py = image2world(homography, det_x, det_y)
+        trans_lat, trans_lon = get_obj_pos(mylat, mylon, my_head + angle_offset, px, py)
+        if (det_x > cam_det_px_dict[det_px_dict_key][0]) and (det_x < cam_det_px_dict[det_px_dict_key][1]) and \
+           (det_y > cam_det_px_dict[det_px_dict_key][2]) and (det_y < cam_det_px_dict[det_px_dict_key][3]):
+            cam_obj_gps.append([cam_index, obj_class, trans_lat, trans_lon])
+        cam_obj.append([cam_index, obj_class, det_x, det_y])
+
