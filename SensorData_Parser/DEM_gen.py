522691f5af303bd65c74436a3e2cbe278cfcc721
NOHYC <nyc1517@gmail.com>
2024-11-08 14:00:28 +0900

    add dem

--- SensorData_Parser/DEM_gen.py
+++ SensorData_Parser/DEM_gen.py
@@ -14,3 +14,8 @@ cam01_H = np.load(config["camera_matrices"]["cam01_H"])
 cam02_H = np.load(config["camera_matrices"]["cam02_H"])
 cam03_H = np.load(config["camera_matrices"]["cam03_H"])
 cam04_H = np.load(config["camera_matrices"]["cam04_H"])
+
+# Set constants and dictionaries
+DEG_TO_RAD = config["constants"]["DEG_TO_RAD"]
+canfd_dict = config["canfd_dict"]
+cam_det_px_dict = config["cam_det_px_dict"]
